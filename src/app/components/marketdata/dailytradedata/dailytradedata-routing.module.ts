b1c8fbb5821a438f6db938d6e78de81f731eafe6
robelgh <robelghw123@gmail.com>
2025-01-02 10:33:53 +0300

    dailytrade

--- src/app/components/marketdata/dailytradedata/dailytradedata-routing.module.ts
+++ src/app/components/marketdata/dailytradedata/dailytradedata-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DailytradedataComponent } from './dailytradedata.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild([
+    { path: '', component: DailytradedataComponent }
+])],
   exports: [RouterModule]
 })
 export class DailytradedataRoutingModule { }
